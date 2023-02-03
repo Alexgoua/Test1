82b4f3c5464eafc300afe2e716685e6dbb4a306e
Alexgoua <alexgoua@gmail.com>
2023-02-03 16:37:57 +0200

    my commit message

--- JavaApplication2/src/Lesson15/Test2.java
+++ JavaApplication2/src/Lesson15/Test2.java
@@ -3,9 +3,11 @@ package Lesson15;
 public class Test2 {
     public static void main(String[] args) {
         int i = 1;
+        int a = 5;
         do {
             System.out.println(i);
             i++;
         } while (i <= 10);
+        System.out.println(a);
     }
 }
