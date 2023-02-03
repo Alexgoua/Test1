4957c901ec676cf2bc77fe8df54f7eeb6e4dda43
Alexgoua <alexgoua@gmail.com>
2023-02-03 18:13:57 +0200

    fdsfd

--- JavaApplication2/src/Homework/Lesson15/Test1.java
+++ JavaApplication2/src/Homework/Lesson15/Test1.java
@@ -4,17 +4,23 @@ public class Test1 {
     public static void main(String[] args) {
         int hour = -1;
 
-        HOURS: while (hour < 24) {
+        HOURS: while (hour < 5) {
             hour++;
             int minute = -1;
             MINUTES: do {
+                if (hour > 1 && minute / 10 > 0 && minute % 10 == 0) {
+                    break HOURS;
+                }
                 minute++;
                 int second = -1;
-                SECONDS: while (second < 60) {
+                SECONDS: while (second < 59) {
                     second++;
                     System.out.println(hour + ":" + minute + ":" + second);
+                    if (second * hour > minute) {
+                        break;
+                    }
                 }
-            } while (minute < 60);
+            } while (minute <59);
         }
     }
 }
