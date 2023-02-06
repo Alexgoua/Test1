cf114ff4ab0f3008c8131d43ce8205914d4882d9
Alexgoua <alexgoua@gmail.com>
2023-02-06 19:46:13 +0200

    06.02 Lesson16

--- JavaApplication2/src/Homework/Lesson16/Email.java
+++ JavaApplication2/src/Homework/Lesson16/Email.java
@@ -1,16 +1,13 @@
 package Homework.Lesson16;
 
 public class Email {
-    static void email(String string) {
-        char c;
-        int i = 0;
-        OUTER: for (; i < string.length(); i++) {
-            //c = string.charAt(i);
-            if (string.charAt(i) == '@') {
-                while (string.charAt(i) != '.')
-                 {
+    static void email(String s) {
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '@') {
+                i++;
+                while (s.charAt(i) != '.') {
+                    System.out.print(s.charAt(i));
                     i++;
-                    System.out.print(string.charAt(i));
                 }
                 System.out.println();
             }
@@ -18,7 +15,6 @@ public class Email {
     }
     public static void main(String[] args) {
         String s = "test1@yopmail.com; user34@yahoo.com; ona@gmail.com;";
-
         email(s);
     }
 }
