62fb93bda734a65815728c8af26d74dc9df2cb74
Alexgoua <alexgoua@gmail.com>
2023-02-10 16:58:06 +0200

    10.02 Lesson19 hw

---
+++ JavaApplication2/src/Homework/Lesson19/Test2.java
@@ -0,0 +1,45 @@
+package Homework.Lesson19;
+
+public class Test2 {
+    static String[] abc(String[] ... s) {
+        int count = 0;
+        for (String[] array1: s) {
+            for (String s1: array1) {
+                count++;
+            }
+        }
+        String[] array = new String[count];
+        count = 0;
+        for (String[] array1: s) {
+            for (String s1: array1) {
+                array[count] = s1;
+                count++;
+            }
+        }
+        for (String s2: array) {
+            System.out.print(s2 + " ");
+        }
+        return array;
+    }
+    public static void main(String[] args) {
+        String[] s1 = {"aaa", "bbb", "ccc"};
+        String[] s2 = {"dd", "ee", "ff", "gg", "hh"};
+        String[] s3 = {"iiii", "jjjj"};
+
+        String[] arrayNew = abc(s1, s2, s3);
+        String[] args1 = {"bbb", "iiii", "jjjj"};
+
+        for (int i = 0; i < arrayNew.length; i++) {
+            for (String s5: args) {
+                if (arrayNew[i].equals(s5)) {
+                    arrayNew[i] = null;
+                    break;
+                }
+            }
+        }
+        System.out.println();
+        for (String s: arrayNew) {
+            System.out.print(s + " ");
+        }
+    }
+}
--- JavaApplication2/src/Lesson19/Test1.java
+++ JavaApplication2/src/Lesson19/Test1.java
@@ -1,6 +1,6 @@
 package Lesson19;
 
-public class Test1 {
+public class Test1 { // для запуска с консоли
     public static void main(String[] args) {
         System.out.println("Nulevoi element massiva: " + args[0]);
         System.out.println("Dlina massiva: " + args.length);
